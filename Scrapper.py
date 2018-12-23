f4bdfde707d32667d767003aa15d8544f8db4776
王源 <wangyuan@wangyuandeMacBook-Pro.local>
2018-12-23 13:54:09 -0700

    update picture filename

--- Scrapper.py
+++ Scrapper.py
@@ -11,21 +11,22 @@ class Scrapper:
 		self.agent = UserAgent()
 		self.headers = {"User-Agent":self.agent.random}
 		self.path = path
-		self.pattern = re.compile(r'(?<=\").+?(?=\?)', re.S)
+		self.linkPattern = re.compile(r'(?<=\").+?(?=\?)', re.S)
+		self.namePattern = re.compile(r'\d+',re.S)
 
 	def readHtml(self,url):
 
 		req = Request(url = url,headers = self.headers)
-		html=urlopen(req,timeout=5)
-		bj=BeautifulSoup(html.read(),"lxml")
-		content=bj.findAll("img",{"srcset":True})
+		html = urlopen(req,timeout=5)
+		bj = BeautifulSoup(html.read(),"lxml")
+		content = bj.findAll("img",{"srcset":True})
 		for i in content:
-			imgLink = self.pattern.findall(i['src'])[0]
+			imgLink = self.linkPattern.findall(i['src'])[0]
 			self.downLoad(imgLink)
 
 	def downLoad(self,img):
-
-		name="/" + img.replace(':', '@').replace('/', '_')
+		number = self.namePattern.findall(img)[0]
+		name = number + ".jpeg"
 		req = requests.get(img,headers = self.headers)
 		pic = req.content
 		with open(self.path+name,'wb') as f:
