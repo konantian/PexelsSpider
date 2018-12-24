d1719bbab2aa736f902984af849b8be128a8d1c4
Yuan Wang <wang17@ualberta.ca>
2018-12-23 21:06:03 -0700

    update user interface

--- Scrapper.py
+++ Scrapper.py
@@ -1,28 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.request import*
+from urllib.request import *
 from fake_useragent import UserAgent
 import re
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class Scrapper:
 
-	def __init__(self,path):
+	def __init__(self,path,amount=None):
 
 		self.agent = UserAgent()
 		self.headers = {"User-Agent":self.agent.random}
 		self.path = path
 		self.linkPattern = re.compile(r'(?<=\").+?(?=\?)', re.S)
 		self.namePattern = re.compile(r'\d+',re.S)
+		self.amount = amount
+		self.imgLinks = []
 
-	def readHtml(self,url):
+	def readHtml(self,page):
 
+		url="https://www.pexels.com/?format=js&page={page}".format(page=page)
 		req = Request(url = url,headers = self.headers)
 		html = urlopen(req,timeout=5)
 		bj = BeautifulSoup(html.read(),"lxml")
 		content = bj.findAll("img",{"srcset":True})
 		for i in content:
 			imgLink = self.linkPattern.findall(i['src'])[0]
-			self.downLoad(imgLink)
+			self.imgLinks.append(imgLink)
+			if len(self.imgLinks) == self.amount:
+				return self.imgLinks
+
+		return self.readHtml(page+1)
 
 	def downLoad(self,img):
 		number = self.namePattern.findall(img)[0]
@@ -31,3 +41,4 @@ class Scrapper:
 		pic = req.content
 		with open(self.path+name,'wb') as f:
 			f.write(pic)
+
--- home.py
+++ home.py
@@ -4,19 +4,20 @@
 from multiprocessing.dummy import Pool as ThreadPool
 import os
 from Scrapper import Scrapper
+from tqdm import tqdm
 
 if __name__ == "__main__":
 
 	urls = []
-	firstPage = int(input("Please enter first page: "))
-	endPage = int(input("Please enter end page: "))
-	for i in range(firstPage,endPage+1):
+	numPics = int(input("How many wallpapers do you want to download: "))
+	for i in range(1,(numPics//30)+2):
 		url="https://www.pexels.com/?format=js&page={page}".format(page=i)
 		urls.append(url)
 
 	path = r"./Wallpapers/"
 	if not os.path.exists(path):
 		os.makedirs(path)
-	scrapper = Scrapper(path)
+	scrapper = Scrapper(path,numPics)
+	imgLinks = scrapper.readHtml(2)
 	pool = ThreadPool(20)
-	pool.map(scrapper.readHtml,urls)
+	pool.map(scrapper.downLoad,imgLinks)
